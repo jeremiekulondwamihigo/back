8f8163c95c284ecd08094cca74da3a0a80f397f5
unknown <jeremie2018mihigo@gmail.com>
2023-01-25 01:14:22 +0200

    second erreur

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(200).json({ message: 'Something broke!', error: true })
 })
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  next()
+})
 
 app.use(express.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
